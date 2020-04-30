e04149abe276918950cf0e84d2e5c6d9563b14b6
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-04-30 12:43:59 +0900

    初期設定

---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export const Main = props => {
+  return (
+    <View style={styles.container}>
+      <Text>Main.js</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
