e6db7ab22c8da9317ac0df77324c406a230f502c
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-04-30 13:38:28 +0900

    DrawerContentを作成

--- src/RootNavigator.js
+++ src/RootNavigator.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import { createDrawerNavigator, DrawerContent } from '@react-navigation/drawer'
-
-import { StackNavigator } from './StackNavigator';
-import { DrawerCOntent } from './DrawerContent';
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { StackNavigator } from "./StackNavigator";
+import { DrawerContent } from "./DrawerContent";
 
 const Drawer = createDrawerNavigator();
 
 export const RootNavigator = () => {
   return (
-    <Drawer.Navigator drawerContent = {props => <DrawerContent { ...props} />}>
-      <Drawer.Screen name = "Home" component={StackNavigator} />
+    <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
+      <Drawer.Screen name="Home" component={StackNavigator} />
     </Drawer.Navigator>
-  )
-}
+  );
+};
