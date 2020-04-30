3853ecb1010e77aa210797216cd4c4c80a71f9ff
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-04-30 20:21:28 +0900

    色を統一

--- src/BottomTabNavigator.js
+++ src/BottomTabNavigator.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { creteMaterialBottomTabNavigator, createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { useTheme } from 'react-native-paper';
+import color from 'color';
 
 import { Feed } from './Feed';
 import { Messages } from './Messages';
@@ -8,31 +10,43 @@ import { Notifications } from './Notifications';
 const Tab = createMaterialBottomTabNavigator();
 
 export const BottomTabNavigator = () => {
+  const theme = useTheme();
+  const tabBarColor = theme.dark
+  ? overlay(6, theme.colors.surface)
+  : theme.colors.surface
+
   return (
     <Tab.Navigator
       initialRouteName="Feed"
       shifting={true}
       sceneAnimationEnabled={false}
+      backBehavior='initialRoute'
+      activeColor={theme.colors.text}
+      inactiveColor={color(theme.colors.text)
+        .alpha(0.6)
+        .rgb()
+        .string()
+      }
     >
       <Tab.Screen
         name="Feed"
         component={Feed}
         options={{
-          tabBarIcon: 'home-account'
+          tabBarIcon: 'home-account', tabBarColor
         }}
       />
       <Tab.Screen
         name="Notifications"
         component={Notifications}
         options={{
-          tabBarIcon: 'bell-outline'
+          tabBarIcon: 'bell-outline', tabBarColor
         }}
       />
       <Tab.Screen
         name="Messages"
         component={Messages}
         options={{
-          tabBarIcon: 'message-text-outline'
+          tabBarIcon: 'message-text-outline', tabBarColor
         }}
       />
     </Tab.Navigator>
