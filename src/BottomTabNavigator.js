1182edaaebe4a1a109073e1ffeb288d9d40cd0bc
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-05-01 22:09:25 +0900

    FABのアイコンを追加

--- src/BottomTabNavigator.js
+++ src/BottomTabNavigator.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { creteMaterialBottomTabNavigator, createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { useTheme } from 'react-native-paper';
+import { useTheme, Portal, FAB } from 'react-native-paper';
 import color from 'color';
 
 import { Feed } from './Feed';
@@ -16,39 +16,51 @@ export const BottomTabNavigator = () => {
   : theme.colors.surface
 
   return (
-    <Tab.Navigator
-      initialRouteName="Feed"
-      shifting={true}
-      sceneAnimationEnabled={false}
-      backBehavior='initialRoute'
-      activeColor={theme.colors.text}
-      inactiveColor={color(theme.colors.text)
-        .alpha(0.6)
-        .rgb()
-        .string()
-      }
-    >
-      <Tab.Screen
-        name="Feed"
-        component={Feed}
-        options={{
-          tabBarIcon: 'home-account', tabBarColor
-        }}
-      />
-      <Tab.Screen
-        name="Notifications"
-        component={Notifications}
-        options={{
-          tabBarIcon: 'bell-outline', tabBarColor
-        }}
-      />
-      <Tab.Screen
-        name="Messages"
-        component={Messages}
-        options={{
-          tabBarIcon: 'message-text-outline', tabBarColor
-        }}
-      />
-    </Tab.Navigator>
+    <React.Fragment>
+      <Tab.Navigator
+        initialRouteName="Feed"
+        shifting={true}
+        sceneAnimationEnabled={false}
+        backBehavior='initialRoute'
+        activeColor={theme.colors.text}
+        inactiveColor={color(theme.colors.text)
+          .alpha(0.6)
+          .rgb()
+          .string()
+        }
+      >
+        <Tab.Screen
+          name="Feed"
+          component={Feed}
+          options={{
+            tabBarIcon: 'home-account', tabBarColor
+          }}
+        />
+        <Tab.Screen
+          name="Notifications"
+          component={Notifications}
+          options={{
+            tabBarIcon: 'bell-outline', tabBarColor
+          }}
+        />
+        <Tab.Screen
+          name="Messages"
+          component={Messages}
+          options={{
+            tabBarIcon: 'message-text-outline', tabBarColor
+          }}
+        />
+      </Tab.Navigator>
+      <Portal>
+        <FAB
+          icon='feather'
+          style={{
+            position: 'absolute',
+            bottom: 100,
+            right: 16
+          }}
+        />
+      </Portal>
+    </React.Fragment>
   );
 };
