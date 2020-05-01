18c082a35979799109c09fd554038b0f53b0ed8f
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-05-01 23:38:18 +0900

    Profile.jsを追加

---
+++ src/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export const Profile = props => {
+  return (
+    <View>
+      <Text>profile</Text>
+    </View>
+  );
+};
