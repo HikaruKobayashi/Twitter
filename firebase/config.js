8ac2fb6cc413a3f4e25a24a7845e3ffba696d6d0
H-Kobayashi <51415392+HikaruKobayashi@users.noreply.github.com>
2020-06-14 16:13:24 +0900

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyC_70AFawiPYKt4_Orqt95WSFjO7CaD3wU",
-  authDomain: "twitter-76c64.firebaseapp.com",
-  databaseURL: "https://twitter-76c64.firebaseio.com",
-  projectId: "twitter-76c64",
-  storageBucket: "twitter-76c64.appspot.com",
-  messagingSenderId: "64417237136",
-  appId: "1:64417237136:web:24e188b20c6795e3c53015"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 }
